d934557cfb3082e83937ae6c72848fb2cd649beb
carlosLGM <100099447+carlosLGM@users.noreply.github.com>
2022-09-18 23:25:29 -0600

    guias

---
+++ guia1/ejercicio extra 3/js/convertir.js
@@ -0,0 +1,3 @@
+var celcius = prompt('introduzca los grados en celcius');
+
+document.write('<div class="contenedor">los grados en fahrenheit son:'+((celcius*9/5)+32)+"</div>");
---
+++ guia1/ejericio extra/js/partes.js
@@ -0,0 +1,6 @@
+document.write("<header><p> header </p></header>");
+document.write("<div id='contenedor1'><div id='contenedor2'><nav> navigation </nav></div>");
+document.write("<div id='contenedor3'><article> article </article>");
+document.write("<article> article </article>");
+document.write("<article> article </article></div></div>");
+document.write("<footer> footer </footer>");
